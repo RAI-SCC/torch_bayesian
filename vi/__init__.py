e291197e309dcabef1be0c11c4c6b83b3821c15f
Arvid Weyrauch <arvid.weyrauch@kit.edu>
2024-09-20 13:12:25 +0200

    Add subpackages to pyproject.toml

--- vi/__init__.py
+++ vi/__init__.py
@@ -1,6 +1,5 @@
 """This module provides basic layers and loss functions for BNN-training with Variational Inference."""
 
-from . import predictive_distributions, priors, utils, variational_distributions
 from .base import VIBaseModule, VIModule
 from .conv import VIConv1d, VIConv2d, VIConv3d
 from .kl_loss import KullbackLeiblerLoss
@@ -8,10 +7,6 @@ from .linear import VILinear
 from .sequential import VIResidualConnection, VISequential
 
 __all__ = [
-    "predictive_distributions",
-    "priors",
-    "utils",
-    "variational_distributions",
     "VIModule",
     "VIBaseModule",
     "VILinear",
