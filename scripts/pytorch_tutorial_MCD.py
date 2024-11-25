aef8b24ae67b776aabd87438e01b489b2abe0c3a
AsenaKarolin <asenaozdemirkabatas@gmail.com>
2024-11-25 16:23:41 +0100

    Sample parallel MCD implemented.

--- scripts/pytorch_tutorial_MCD.py
+++ scripts/pytorch_tutorial_MCD.py
@@ -1,22 +1,31 @@
 import time
 from mcd_with_vi import torch_tutorial_MCD
 from standard_fully_connected import torch_standard
+from sample_parallel_mcd import sample_parallel_mcd
 from mcd_regular import torch_MCD_regular
 import torch
+from mpi4py import MPI
 
 
 if __name__ == "__main__":
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+
     input_length = 50
     output_length = 10
     hidden1 = 40
     hidden2 = 20
     batch_size = 32
-    epochs = 10
-    torch.manual_seed(42)
+    epochs = 2
+    general_seed = 42
 
+    print(rank)
+    torch.manual_seed(general_seed)
+
+    sample_parallel_mcd(input_length, hidden1, hidden2, output_length, batch_size, epochs)
     #torch_MCD_regular(input_length, hidden1, hidden2, output_length, batch_size, epochs)
     #start_time = time.time()
-    torch_tutorial_MCD(input_length, hidden1, hidden2, output_length, batch_size, epochs)
+    #torch_tutorial_MCD(input_length, hidden1, hidden2, output_length, batch_size, epochs)
     #print("--- %s seconds ---" % (time.time() - start_time))
     #start_time = time.time()
     #torch_standard(input_length, hidden1, hidden2, output_length, batch_size, epochs)
---
+++ scripts/sample_parallel_mcd.py
@@ -0,0 +1,225 @@
+import torch
+from torch import Tensor, nn
+from torch.nn import MSELoss
+from entsoe_data_load import TimeseriesDataset
+from torch.utils.data import DataLoader
+import polars as pl
+from typing import Callable
+import vi
+from vi import VIModule
+from vi.variational_distributions import MeanFieldNormalVarDist
+from mean_std_plot import sigma_weight_plot
+from random_sample_plot import plot_random_samples
+from mpi4py import MPI
+import numpy as np
+sampling_state = None
+
+def check_variable_consistency(value, want_difference):
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+
+    # Gather all values at root process
+    all_values = comm.gather(value, root=0)
+
+    # Check consistency on the root process
+    if rank == 0:
+        all_values_tensor = torch.stack(all_values, dim=0)
+        if want_difference:
+            if (all_values_tensor[0]==all_values_tensor).all():
+                raise ValueError(f"All samples are the same: {all_values}")
+        else:
+            if not (all_values_tensor[0]==all_values_tensor).all():
+                raise ValueError(f"Inconsistent values across processes: {all_values}")
+
+def test_same_data_load(x, y):
+    check_variable_consistency(x, False)
+    check_variable_consistency(y, False)
+    return
+
+def test_different_samples(samples):
+    check_variable_consistency(samples, True)
+    return
+
+def test_same_current_model(model):
+    for name, param in model.named_parameters():
+        check_variable_consistency(param, False)
+    return
+
+
+
+def sample_parallel_mcd(input_length, hidden1, hidden2, output_length, batch_size, epochs) -> None:
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    world_size = comm.Get_size()
+    enable_tests = True
+
+    global sampling_state
+    sampling_state = None
+
+    df = pl.read_csv("data/ENTSOEEnergyLoads/de.csv",
+        dtypes={"start": pl.Datetime, "end": pl.Datetime, "load": pl.Float32},
+    )
+    x = df["load"]
+    x = x.fill_null(strategy="backward")
+    normalized_x = (x - x.mean()) / x.std()
+    x_tensor = normalized_x.to_torch()
+    data_train, data_test = x_tensor[: int(len(x) * 0.7)], x_tensor[int(len(x) * 0.7):]
+    dataset_train = TimeseriesDataset(data_train, input_length, output_length)
+    dataset_test = TimeseriesDataset(data_test, input_length, output_length)
+
+    # Create data loaders.
+    train_dataloader = DataLoader(dataset=dataset_train, batch_size=batch_size, shuffle=True)
+    test_dataloader = DataLoader(dataset=dataset_test, batch_size=batch_size, shuffle=True)
+
+    # Get cpu, gpu or mps device for training.
+    device = (
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps"
+        if torch.backends.mps.is_available()
+        else "cpu"
+    )
+    print(f"Using {device} device")
+
+    # Define model
+    class NeuralNetwork(vi.VIModule):
+        def __init__(self, variational_distribution=MeanFieldNormalVarDist()) -> None:
+            super().__init__()
+            self.flatten = nn.Flatten()
+            self.linear_relu_stack = vi.VISequential(
+                vi.VILinear(input_length, hidden1, variational_distribution=variational_distribution),
+                nn.ReLU(),
+                vi.VILinear(hidden1, hidden2, variational_distribution=variational_distribution),
+                nn.ReLU(),
+                vi.VILinear(hidden2, output_length, variational_distribution=variational_distribution),
+            )
+
+        def forward(self, x_: Tensor) -> Tensor:
+            x_ = self.flatten(x_)
+            logits = self.linear_relu_stack(x_)
+            return logits
+
+    model = NeuralNetwork(variational_distribution=MeanFieldNormalVarDist(initial_std=1.)).to(device)
+    model.return_log_probs(False)
+    print(model)
+
+    loss_fn = vi.MeanSquaredErrorLoss()
+    #optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+    optimizer = torch.optim.Adam(params=model.parameters(), lr=1e-3, weight_decay=0)
+
+
+    def train(
+        dataloader: DataLoader,
+        model: VIModule,
+        loss_fn: Callable,
+        optimizer: torch.optim.Optimizer,
+    ):
+        global sampling_state
+        #size = len(dataloader.dataset)
+        model.train()
+        for batch, (x, y) in enumerate(dataloader):
+            if enable_tests:
+                test_same_data_load(x, y)
+            x, y = x.to(device), y.to(device)
+            # Compute prediction error
+            regular_state = torch.get_rng_state()
+            if sampling_state == None:
+                torch.manual_seed(rank)
+            else:
+                torch.set_rng_state(sampling_state)
+            if enable_tests:
+                test_same_current_model(model)
+            pred = model(x)
+            if enable_tests:
+                test_different_samples(pred)
+            sampling_state = torch.get_rng_state()
+            torch.set_rng_state(regular_state)
+
+            loss = loss_fn(pred, y)
+            # Backpropagation
+            loss.backward()
+            for param in model.parameters():
+                if param.grad is not None:
+                    # Convert gradients to a NumPy array for MPI communication
+                    grad_numpy = param.grad.data.numpy()
+
+                    # Create an array to hold the averaged gradients
+                    grad_global = np.zeros_like(grad_numpy)
+
+                    # Perform all_reduce to sum gradients across all processes
+                    comm.Allreduce(grad_numpy, grad_global, op=MPI.SUM)
+
+                    # Average the gradients
+                    grad_global /= world_size
+
+                    # Copy the averaged gradients back to the parameter
+                    param.grad.data = torch.tensor(grad_global, dtype=param.grad.data.dtype)
+
+            optimizer.step()
+            optimizer.zero_grad()
+
+
+           # if batch % 100 == 0:
+           #     loss, current = loss.item(), (batch + 1) * len(x)
+           #     print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+
+    def test(dataloader: DataLoader, model: VIModule, loss_fn: Callable) -> None:
+        num_batches = len(dataloader)
+        model.eval()
+        test_loss = 0.0
+        global sampling_state
+        with torch.no_grad():
+            for x, y in dataloader:
+                if enable_tests:
+                    test_same_data_load(x, y)
+                x, y = x.to(device), y.to(device)
+                regular_state = torch.get_rng_state()
+                if sampling_state == None:
+                    torch.manual_seed(rank)
+                else:
+                    torch.set_rng_state(sampling_state)
+                if enable_tests:
+                    test_same_current_model(model)
+                samples = model(x)
+                if enable_tests:
+                    test_different_samples(samples)
+                sampling_state = torch.get_rng_state()
+                torch.set_rng_state(regular_state)
+
+                samples_numpy = samples.numpy()
+
+                # Create an array to hold the averaged gradients
+                samples_global = np.zeros_like(samples_numpy)
+                comm.Allreduce(samples_numpy, samples_global, op=MPI.SUM)
+                samples_global /= world_size
+
+                # Copy the averaged gradients back to the parameter
+                samples = torch.tensor(samples_global, dtype=samples.dtype)
+
+                test_loss += loss_fn(samples, y).item()
+
+        test_loss /= num_batches
+        print(
+            f"Test Error: Avg loss: {test_loss:>8f} \n"
+        )
+
+    for t in range(epochs):
+        print(f"Epoch {t + 1}\n-------------------------------")
+        train(train_dataloader, model, loss_fn, optimizer)
+
+        test(test_dataloader, model, loss_fn)
+
+    print("Done!")
+
+'''
+    for name, param in model.named_parameters():
+        if param.requires_grad:
+            if "log_std" in name:
+                base = name.removesuffix('log_std')
+                weight_layer_name = base + "mean"
+                weight_param = dict(model.named_parameters())[weight_layer_name]
+                sigma_weight_plot(weight_param, param, base)
+
+    plot_random_samples(model, test_dataloader)
+'''
