ba49f273f87ab4615abb5f114f9c5b4559d239ad
Arvid Weyrauch <arvid.weyrauch@kit.edu>
2026-03-12 08:57:21 +0100

    Update version number, add Python 3.14 tests to workflow

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ sys.path.insert(0, os.path.abspath("../../torch_blue"))
 project = "torch_blue"
 copyright = "2026, RAI-SCC"
 author = "RAI-SCC"
-release = "0.9.2"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
