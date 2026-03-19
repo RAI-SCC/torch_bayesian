d5683444ce862e228dcaa9ae6a9e3fbd8f75a364
Arvid Weyrauch <arvid.weyrauch@kit.edu>
2026-03-19 15:12:55 +0100

    Add epsilon to predictive log prob

--- torch_blue/vi/distributions/normal.py
+++ torch_blue/vi/distributions/normal.py
@@ -188,9 +188,8 @@ class MeanFieldNormal(Distribution):
         std = samples.std(dim=0)
         return mean, std
 
-    @staticmethod
     def log_prob_from_parameters(
-        reference: Tensor, parameters: Tuple[Tensor, Tensor]
+        self, reference: Tensor, parameters: Tuple[Tensor, Tensor]
     ) -> Tensor:
         """
         Calculate the log probability of reference given the predictive mean and standard deviation.
@@ -214,7 +213,7 @@ class MeanFieldNormal(Distribution):
             Shape: (1,).
         """
         mean, std = parameters
-        variance = std**2
+        variance = std**2 + self.eps
         data_fitting = (reference - mean) ** 2 / variance
         normalization = torch.log(variance)
         if _globals._USE_NORM_CONSTANTS:
